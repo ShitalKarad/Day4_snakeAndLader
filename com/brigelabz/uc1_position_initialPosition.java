5db0c63da27d3440eb7ad040a44e6259ab832565
ShitalKarad <shitalkarad24@gmail.com>
2022-11-23 17:05:13 +0530

    Added:uc1 welcome message and starting position

---
+++ com/brigelabz/uc1_position_initialPosition.java
@@ -0,0 +1,13 @@
+package com.brigelabz;
+
+public class uc1_position_initialPosition {
+    static final int initialPosition = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome in snake ladder program");
+
+        System.out.println("current position "+initialPosition);
+
+    }
+
+}
